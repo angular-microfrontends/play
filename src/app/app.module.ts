8d16dd2025d284a4d30b6ad9d739f03157eac515
daniloesk <47666332+daniloesk@users.noreply.github.com>
2021-01-30 08:21:23 -0300

    Shared service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '@angular-mf/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { EmptyRouteComponent } from './empty-route/empty-route.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/attack/attack.component.ts
+++ src/app/attack/attack.component.ts
@@ -1,8 +1,16 @@
+import { core } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { CoreService } from '@angular-mf/core';
 
 @Component({
   selector: 'play-attack',
   templateUrl: './attack.component.html',
   styleUrls: ['./attack.component.sass'],
 })
-export class AttackComponent { }
+export class AttackComponent {
+  public message = this.core.readMessage();
+
+  constructor(
+    private core: CoreService,
+  ) { }
+}
